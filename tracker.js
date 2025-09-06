a1da9e973a3cccfbef15a1aa70cb94a0eebefc31
jainam <pateljainams122@gmail.com>
2025-09-06 18:40:08 +0530

    added timer also

--- tracker.js
+++ tracker.js
@@ -1,39 +1,62 @@
 import activeWin from "active-win";
-import {getAppIconByPid} from '@lwtlab/node-mac-app-icon'
+import { getAppIconByPid } from '@lwtlab/node-mac-app-icon';
+
 const API_URL = "https://background-app-tracker.onrender.com/update";
-let last_app = null
+let last_app = null;
+let last_switch_time =Date.now()
 
-setInterval(async()=>{
-try {
-    const win =await activeWin()
-    if(win && win.owner.name !== last_app){
-        last_app = win.owner.name
-        let iconBase64 = null
-        const pid = win.owner.processId;
-        if(pid){
-         try {
-               const iconBuffer = await getAppIconByPid(pid);
-            iconBase64 = iconBuffer.toString('base64');
-            console.log(`Fetched icon for: ${last_app}`);
-            console.log(iconBase64)
+
+setInterval(async () => {
+    try {
+        const win = await activeWin();
+        let currentApp = null;
+        let iconBase64 = null;
+
+        
+        if (win) {
+            currentApp = win.owner.name;
+            const pid = win.owner.processId;
             
-         } catch (error) {
-            console.error("Error fetching icon:", error.message);
-         }
-        }
-          const payload = {
-        activeApp: last_app,
-        icon: iconBase64
-      };
+            
+            if (pid) {
+                try {
+                    const iconBuffer = await getAppIconByPid(pid);
+                    iconBase64 = iconBuffer.toString('base64');
+                    console.log(`Fetched icon for: ${currentApp}`);
+                } catch (error) {
+                    console.error("Error fetching icon:", error.message);
+                }
+            }
+            if(currentApp !== last_app){
 
-        await fetch(API_URL,{
-             method: "POST",
-        headers: { "Content-Type": "application/json" },
-         body: JSON.stringify(payload),
-        })
-         console.log("Updated:", last_app);
+            }
+        } else {
+           
+            currentApp = "Offline";
+            iconBase64 = null; 
+        }
+
+      
+        if (currentApp !== last_app) {
+            last_app = currentApp;
+            last_switch_time= Date.now()
+
+            const payload = {
+                activeApp: last_app,
+                icon: iconBase64,
+                startTime: last_switch_time
+            };
+
+            await fetch(API_URL, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(payload),
+            });
+
+            console.log("Updated:", last_app);
+        }
+
+    } catch (error) {
+        console.error("Error:", error.message);
     }
-} catch (error) {
-    console.error("Error:", error.message);
-}
-},30000)
+}, 30000);
